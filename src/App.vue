c3faa98cbc35de518c3948d98d1f1ac556a5d858
muhammad-isroq <muhammadisroq7@gmail.com>
2024-11-20 19:43:10 +0700

    instalasi vue js

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MainButton from './components/MainButton.vue'
+const name = 'isroq'
+</script>
+
+<template>
+  <h1>{{name}}</h1>
+<MainButton/>
+<MainButton/>  
+<MainButton/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainButton.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div>
+        <button>button</button>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+    
+</style>
